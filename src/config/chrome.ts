d3aaa035226c55ba7f9fb7ba92a5f5f1ce3aecad
j-mendez <jeff@a11ywatch.com>
2022-05-10 12:44:56 -0400

    chore(dns): add dns lookup for docket networks

--- src/config/chrome.ts
+++ src/config/chrome.ts
@@ -1,46 +1,58 @@
 import { get } from "http";
+import dns from "dns";
 
-let chromeHost = process.env.CHROME_HOST || "127.0.0.1";
-
-const getWs = (host?: string): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    get(`http://${host || chromeHost}:9222/json/version`, (res) => {
-      res.setEncoding("utf8");
-      let rawData = "";
-
-      res.on("data", (chunk) => {
-        rawData += chunk;
-      });
-
-      res.on("end", () => {
-        let data;
-        try {
-          data = JSON.parse(rawData);
-        } catch (e) {
-          console.error(e);
-        }
-        resolve(data?.webSocketDebuggerUrl);
-      });
-    }).on("error", (err) => {
-      reject(
-        `${err.message}: Retrying with docker host targets. Set the env variable of 'CHROME_HOST' before to bypass retries.`
-      );
-    });
-  });
-};
-
+// the chrome hostname dns to connect to with lighthouse sockets
+let chromeHost = process.env.CHROME_HOST;
 // the chrome socket connection to connect to
 let wsChromeEndpointurl = process.env.CHROME_SOCKET_URL;
+
+const getWs = (host?: string): Promise<string> => {
+  return new Promise(async (resolve, reject) => {
+    const defaultHost = host || chromeHost || "127.0.0.1";
+
+    const lookupChromeHost = (target?: string) => {
+      get(`http://${target || defaultHost}:9222/json/version`, (res) => {
+        res.setEncoding("utf8");
+        let rawData = "";
+
+        res.on("data", (chunk) => {
+          rawData += chunk;
+        });
+
+        res.on("end", () => {
+          let data;
+          try {
+            data = JSON.parse(rawData);
+          } catch (e) {
+            console.error(e);
+          }
+          resolve(data?.webSocketDebuggerUrl);
+        });
+      }).on("error", (err) => {
+        reject(
+          `${err.message}: Retrying with docker host targets. Set the env variable of 'CHROME_HOST' before to bypass retries.`
+        );
+      });
+    };
+
+    // add dns lookup from network
+    if (!chromeHost) {
+      dns.lookup("chrome", (err, address, family) => {
+        console.log("address: %j family: IPv%s", address, family);
+        chromeHost = address;
+        lookupChromeHost(address);
+      });
+    } else {
+      lookupChromeHost();
+    }
+  });
+};
 
 export const getWsEndPoint = async (retry?: boolean) => {
   try {
     let retryHost = !retry ? "docker.for.mac.localhost" : "";
     // retry connection on as mac localhost
     wsChromeEndpointurl = await getWs(retryHost);
-    // if docker host valid set the default host for lighthouse
-    if (!retry && wsChromeEndpointurl) {
-      chromeHost = "docker.for.mac.localhost";
-    }
   } catch (e) {
     console.error(e);
   }
