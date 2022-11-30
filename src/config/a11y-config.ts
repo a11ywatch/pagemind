843abbadb085920349cb5f6f8e56c13f18e4f4ed
j-mendez <jeff@a11ywatch.com>
2022-11-30 09:07:28 -0500

    perf(scan): add single digit timeouts

--- src/config/a11y-config.ts
+++ src/config/a11y-config.ts
@@ -2,5 +2,5 @@ export const a11yConfig = {
   includeNotices: false,
   includeWarnings: true,
   chromeLaunchConfig: { ignoreHTTPSErrors: true, executablePath: undefined },
-  timeout: 15000,
+  timeout: 9500,
 };
